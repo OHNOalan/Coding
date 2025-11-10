a3fdced295c109f60c25f3164b3e64486a58f9c1
leealan <leealan@tencent.com>
2025-11-09 22:39:22 -0800

    update

---
+++ history/cf2164F2.cpp
@@ -0,0 +1,498 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+
+template<class T>
+constexpr T power(T a, u64 b, T res = 1) {
+    for (; b != 0; b /= 2, a *= a) {
+        if (b & 1) {
+            res *= a;
+        }
+    }
+    return res;
+}
+
+template<u32 P>
+constexpr u32 mulMod(u32 a, u32 b) {
+    return u64(a) * b % P;
+}
+
+template<u64 P>
+constexpr u64 mulMod(u64 a, u64 b) {
+    u64 res = a * b - u64(1.L * a * b / P - 0.5L) * P;
+    res %= P;
+    return res;
+}
+
+constexpr i64 safeMod(i64 x, i64 m) {
+    x %= m;
+    if (x < 0) {
+        x += m;
+    }
+    return x;
+}
+
+constexpr std::pair<i64, i64> invGcd(i64 a, i64 b) {
+    a = safeMod(a, b);
+    if (a == 0) {
+        return {b, 0};
+    }
+
+    i64 s = b, t = a;
+    i64 m0 = 0, m1 = 1;
+
+    while (t) {
+        i64 u = s / t;
+        s -= t * u;
+        m0 -= m1 * u;
+
+        std::swap(s, t);
+        std::swap(m0, m1);
+    }
+
+    if (m0 < 0) {
+        m0 += b / s;
+    }
+
+    return {s, m0};
+}
+
+template<std::unsigned_integral U, U P>
+struct ModIntBase {
+public:
+    constexpr ModIntBase() : x(0) {}
+    template<std::unsigned_integral T>
+    constexpr ModIntBase(T x_) : x(x_ % mod()) {}
+    template<std::signed_integral T>
+    constexpr ModIntBase(T x_) {
+        using S = std::make_signed_t<U>;
+        S v = x_ % S(mod());
+        if (v < 0) {
+            v += mod();
+        }
+        x = v;
+    }
+
+    constexpr static U mod() { return P; }
+
+    constexpr U val() const { return x; }
+
+    constexpr ModIntBase operator-() const {
+        ModIntBase res;
+        res.x = (x == 0 ? 0 : mod() - x);
+        return res;
+    }
+
+    constexpr ModIntBase inv() const { return power(*this, mod() - 2); }
+
+    constexpr ModIntBase &operator*=(const ModIntBase &rhs) & {
+        x = mulMod<mod()>(x, rhs.val());
+        return *this;
+    }
+    constexpr ModIntBase &operator+=(const ModIntBase &rhs) & {
+        x += rhs.val();
+        if (x >= mod()) {
+            x -= mod();
+        }
+        return *this;
+    }
+    constexpr ModIntBase &operator-=(const ModIntBase &rhs) & {
+        x -= rhs.val();
+        if (x >= mod()) {
+            x += mod();
+        }
+        return *this;
+    }
+    constexpr ModIntBase &operator/=(const ModIntBase &rhs) & { return *this *= rhs.inv(); }
+
+    friend constexpr ModIntBase operator*(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator+(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator-(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+    friend constexpr ModIntBase operator/(ModIntBase lhs, const ModIntBase &rhs) {
+        lhs /= rhs;
+        return lhs;
+    }
+
+    friend constexpr std::istream &operator>>(std::istream &is, ModIntBase &a) {
+        i64 i;
+        is >> i;
+        a = i;
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const ModIntBase &a) { return os << a.val(); }
+
+    friend constexpr bool operator==(const ModIntBase &lhs, const ModIntBase &rhs) { return lhs.val() == rhs.val(); }
+    friend constexpr std::strong_ordering operator<=>(const ModIntBase &lhs, const ModIntBase &rhs) {
+        return lhs.val() <=> rhs.val();
+    }
+
+private:
+    U x;
+};
+
+template<u32 P>
+using ModInt = ModIntBase<u32, P>;
+template<u64 P>
+using ModInt64 = ModIntBase<u64, P>;
+
+struct Barrett {
+public:
+    Barrett(u32 m_) : m(m_), im((u64) (-1) / m_ + 1) {}
+
+    constexpr u32 mod() const { return m; }
+
+    constexpr u32 mul(u32 a, u32 b) const {
+        u64 z = a;
+        z *= b;
+
+        u64 x = u64((u128(z) * im) >> 64);
+
+        u32 v = u32(z - x * m);
+        if (m <= v) {
+            v += m;
+        }
+        return v;
+    }
+
+private:
+    u32 m;
+    u64 im;
+};
+
+template<u32 Id>
+struct DynModInt {
+public:
+    constexpr DynModInt() : x(0) {}
+    template<std::unsigned_integral T>
+    constexpr DynModInt(T x_) : x(x_ % mod()) {}
+    template<std::signed_integral T>
+    constexpr DynModInt(T x_) {
+        int v = x_ % int(mod());
+        if (v < 0) {
+            v += mod();
+        }
+        x = v;
+    }
+
+    constexpr static void setMod(u32 m) { bt = m; }
+
+    static u32 mod() { return bt.mod(); }
+
+    constexpr u32 val() const { return x; }
+
+    constexpr DynModInt operator-() const {
+        DynModInt res;
+        res.x = (x == 0 ? 0 : mod() - x);
+        return res;
+    }
+
+    constexpr DynModInt inv() const {
+        auto v = invGcd(x, mod());
+        assert(v.first == 1);
+        return v.second;
+    }
+
+    constexpr DynModInt &operator*=(const DynModInt &rhs) & {
+        x = bt.mul(x, rhs.val());
+        return *this;
+    }
+    constexpr DynModInt &operator+=(const DynModInt &rhs) & {
+        x += rhs.val();
+        if (x >= mod()) {
+            x -= mod();
+        }
+        return *this;
+    }
+    constexpr DynModInt &operator-=(const DynModInt &rhs) & {
+        x -= rhs.val();
+        if (x >= mod()) {
+            x += mod();
+        }
+        return *this;
+    }
+    constexpr DynModInt &operator/=(const DynModInt &rhs) & { return *this *= rhs.inv(); }
+
+    friend constexpr DynModInt operator*(DynModInt lhs, const DynModInt &rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator+(DynModInt lhs, const DynModInt &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator-(DynModInt lhs, const DynModInt &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+    friend constexpr DynModInt operator/(DynModInt lhs, const DynModInt &rhs) {
+        lhs /= rhs;
+        return lhs;
+    }
+
+    friend constexpr std::istream &operator>>(std::istream &is, DynModInt &a) {
+        i64 i;
+        is >> i;
+        a = i;
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const DynModInt &a) { return os << a.val(); }
+
+    friend constexpr bool operator==(const DynModInt &lhs, const DynModInt &rhs) { return lhs.val() == rhs.val(); }
+    friend constexpr std::strong_ordering operator<=>(const DynModInt &lhs, const DynModInt &rhs) {
+        return lhs.val() <=> rhs.val();
+    }
+
+private:
+    u32 x;
+    static Barrett bt;
+};
+
+template<u32 Id>
+Barrett DynModInt<Id>::bt = 998244353;
+
+using Z = ModInt<998244353>;
+
+struct Comb {
+    int n;
+    std::vector<Z> _fac;
+    std::vector<Z> _invfac;
+    std::vector<Z> _inv;
+
+    Comb() : n{0}, _fac{1}, _invfac{1}, _inv{0} {}
+    Comb(int n) : Comb() { init(n); }
+
+    void init(int m) {
+        if (m <= n) return;
+        _fac.resize(m + 1);
+        _invfac.resize(m + 1);
+        _inv.resize(m + 1);
+
+        for (int i = n + 1; i <= m; i++) {
+            _fac[i] = _fac[i - 1] * i;
+        }
+        _invfac[m] = _fac[m].inv();
+        for (int i = m; i > n; i--) {
+            _invfac[i - 1] = _invfac[i] * i;
+            _inv[i] = _invfac[i] * _fac[i - 1];
+        }
+        n = m;
+    }
+
+    Z fac(int m) {
+        if (m > n) init(2 * m);
+        return _fac[m];
+    }
+    Z invfac(int m) {
+        if (m > n) init(2 * m);
+        return _invfac[m];
+    }
+    Z inv(int m) {
+        if (m > n) init(2 * m);
+        return _inv[m];
+    }
+    Z binom(int n, int m) {
+        if (n < m || m < 0) return 0;
+        return fac(n) * invfac(m) * invfac(n - m);
+    }
+} comb;
+
+namespace std {
+
+    template<class Fun>
+    class y_combinator_result {
+        Fun fun_;
+
+    public:
+        template<class T>
+        explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+        template<class... Args>
+        decltype(auto) operator()(Args &&...args) {
+            return fun_(std::ref(*this), std::forward<Args>(args)...);
+        }
+    };
+
+    template<class Fun>
+    decltype(auto) y_combinator(Fun &&fun) {
+        return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    }
+
+} // namespace std
+
+
+mt19937 mt_treap(4823);
+
+template<typename T>
+struct node {
+    int cnt;
+    T v, lazy;
+    int64_t p;
+
+    node *l, *r;
+    node(T _v) : cnt(1), v(_v), lazy(0), p(mt_treap()), l(nullptr), r(nullptr) {}
+};
+
+template<typename T>
+void push(node<T> *t) {
+    if (t == nullptr) return;
+    if (t->lazy) {
+        if (t->l != nullptr) {
+            t->l->lazy += t->lazy;
+            t->l->v += t->lazy;
+        }
+        if (t->r != nullptr) {
+            t->r->lazy += t->lazy;
+            t->r->v += t->lazy;
+        }
+        t->lazy = 0;
+    }
+}
+
+template<typename T>
+int cnt(node<T> *t) {
+    return t == nullptr ? 0 : t->cnt;
+}
+
+template<typename T>
+void splitv(node<T> *t, node<T> *&l, node<T> *&r, T v) { // splits into <= v and > v
+    if (t == nullptr) {
+        l = nullptr;
+        r = nullptr;
+    } else if (t->v < v) {
+        push(t);
+        splitv(t->r, t->r, r, v);
+        l = t;
+    } else {
+        push(t);
+        splitv(t->l, l, t->l, v);
+        r = t;
+    }
+}
+template<typename T>
+void merge(node<T> *&t, node<T> *l, node<T> *r) {
+    if (l == nullptr) {
+        t = r;
+    } else if (r == nullptr) {
+        t = l;
+    } else if (l->p < r->p) {
+        push(l);
+        merge(l->r, l->r, r);
+        t = l;
+    } else {
+        push(r);
+        merge(r->l, l, r->l);
+        t = r;
+    }
+}
+
+template<typename T>
+void pt(node<T> *r) {
+    if (r == nullptr) return;
+    pt(r->l);
+    cout << r->v << "" << r->p << "" << r->cnt << endl;
+    pt(r->r);
+}
+
+template<typename T>
+void treap_union(vector<pair<T, T>> &store, node<T> *&t, node<T> *l, node<T> *r) {
+    if (l == nullptr) {
+        t = r;
+    } else if (r == nullptr) {
+        t = l;
+    } else {
+        push(l);
+        push(r);
+        if (l->p > r->p) swap(l, r);
+        node<T> *a, *b, *c;
+        splitv(r, a, b, l->v);
+        splitv(b, b, c, l->v + 1);
+        if (b) {
+            store.push_back({l->cnt, b->cnt});
+            l->cnt += b->cnt;
+            delete b;
+        }
+        treap_union(store, l->l, l->l, a);
+        treap_union(store, l->r, l->r, c);
+        t = l;
+    }
+}
+
+void solve() {
+    int N;
+    cin >> N;
+    vector<int> par(N);
+    par[0] = -1;
+    vector<vector<int>> ch(N);
+    for (int i = 1; i < N; i++) {
+        cin >> par[i];
+        par[i]--;
+        ch[par[i]].push_back(i);
+    }
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    int d = 0;
+    Z ans = 1;
+
+    using tnode = node<int>;
+
+    vector<pair<int, int>> store;
+
+    y_combinator([&](auto self, int v) -> tnode * {
+        d++;
+        tnode *freq = nullptr;
+        for (int w: ch[v]) {
+            auto z = self(w);
+            treap_union(store, freq, freq, z);
+        }
+        tnode *a, *l, *r, *b;
+        splitv(freq, a, l, A[v]);
+        splitv(l, l, r, A[v] + 1);
+        splitv(r, r, b, A[v] + 2);
+        int ncnt = cnt(l) + cnt(r) + 1;
+        if (l) delete l;
+        if (r) delete r;
+        l = new node(A[v]);
+        l->cnt = ncnt;
+        if (b) {
+            b->v -= 1;
+            b->lazy -= 1;
+        }
+        merge(freq, a, l);
+        merge(freq, freq, b);
+        d--;
+        return freq;
+    })(0);
+
+    for (auto [x, y]: store) {
+        ans *= comb.binom(x + y, x);
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
